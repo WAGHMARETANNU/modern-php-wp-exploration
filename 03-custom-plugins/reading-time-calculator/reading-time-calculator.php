746757bac63a8b499d8cd14209852a45e8222aff
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-01-21 21:18:37 +0530

    completed reading time plugin with the_content filter and rtc_ prefixing

--- 03-custom-plugins/reading-time-calculator/reading-time-calculator.php
+++ 03-custom-plugins/reading-time-calculator/reading-time-calculator.php
@@ -2,31 +2,36 @@
 /**
  * Plugin Name: Reading Time Calculator
  * Description: A logic-based plugin to estimate reading time.
- * Version: 1.0
+ * Version: 1.1
  * Author: Tannu
  */
 
-// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-/**
- * The core logic is wrapped in a class to stay organized.
- */
 class RTC_Calculator {
 
     public function __construct() {
-        // This is where we will eventually "hook" into WordPress.
+        // We 'hook' our function into 'the_content' filter
+        // 10 is the priority, 1 is the number of arguments
+        add_filter( 'the_content', [ $this, 'display_reading_time' ], 10, 1 );
     }
 
-    public function calculate( $content ) {
+    /**
+     * This function now automatically attaches to the post content
+     */
+    public function display_reading_time( $content ) {
         $word_count = str_word_count( strip_tags( $content ) );
         $m_time = ceil( $word_count / 200 );
 
-        return $m_time;
+        $prefix = "<p style='color: #666; font-style: italic;'>";
+        $prefix .= "⏱️ Estimated reading time: " . $m_time . " min";
+        $prefix .= "</p><hr>";
+
+        // We return the new prefix PLUS the original content
+        return $prefix . $content;
     }
 }
 
-// Initialize the class
 $rtc_calculator = new RTC_Calculator();
